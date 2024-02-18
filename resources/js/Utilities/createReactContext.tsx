89e6bcca519580a20998ffdf78911a028eda1bdd
KimHout Sam <samkimhout@gmail.com>
2024-02-18 10:52:45 +0700

    feat: add `create-react-context`
    closed #2

---
+++ resources/js/Utilities/createReactContext.tsx
@@ -0,0 +1,43 @@
+import React from 'react'
+
+export type CreateReactContext<T extends object> = [
+  {
+    Context: React.Context<T | null>
+    Consumer: React.Consumer<T | null>
+    Provider: React.FC<React.PropsWithChildren<T | null>>
+  },
+  () => T | null,
+]
+
+export function createReactContext<T extends object>(name: string): CreateReactContext<T> {
+  const ReactContext = React.createContext<T | null>(null)
+
+  ReactContext.displayName = name
+
+  const ReactContextConsumer = ReactContext.Consumer
+
+  const ReactContextProvider: React.FC<React.PropsWithChildren<T & {}>> = ({
+    children,
+    ...props
+  }) => {
+    const value = React.useMemo(() => props as T, [props])
+
+    return <ReactContext.Provider value={value}>{children}</ReactContext.Provider>
+  }
+
+  const useReactContext = () => {
+    const context = React.useContext(ReactContext)
+
+    if (typeof context === 'object' && context == null) {
+      console.log(`E_CANNOT_FIND_SCOPE_FOR_PROVIDER_NAME: [${name}]`)
+      return null
+    }
+
+    return context
+  }
+
+  return [
+    { Context: ReactContext, Consumer: ReactContextConsumer, Provider: ReactContextProvider },
+    useReactContext,
+  ]
+}
