cdb208de883de7e40dfd29694facd83f1b1523c1
KimHout Sam <samkimhout@gmail.com>
2024-02-27 15:32:21 +0700

    refactor: export components

--- resources/js/Components/Elements/Header/index.ts
+++ resources/js/Components/Elements/Header/index.ts
@@ -1,10 +1,23 @@
-export { Root } from './Root'
-export { Navigation } from './Navigation'
-export { List } from './List'
-export { Item } from './Item'
-export { Block } from './Block'
-export { Content } from './Content'
-export { Link } from './Link'
-export { Sub } from './Sub'
-export { Trigger } from './Trigger'
-export { Viewport } from './Viewport'
+import { Root } from './Root'
+import { Navigation } from './Navigation'
+import { List } from './List'
+import { Item } from './Item'
+import { Block } from './Block'
+import { Content } from './Content'
+import { Link } from './Link'
+import { Sub } from './Sub'
+import { Trigger } from './Trigger'
+import { Viewport } from './Viewport'
+
+export { Root, Navigation, List, Item, Block, Content, Link, Sub, Trigger, Viewport }
+export default Object.assign(Root, {
+  Navigation,
+  List,
+  Item,
+  Block,
+  Content,
+  Link,
+  Sub,
+  Trigger,
+  Viewport,
+})
