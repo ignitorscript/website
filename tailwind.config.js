cbf8d5738dc71192f346fef5a3a010d03ca620b9
KimHout Sam <samkimhout@gmail.com>
2024-03-03 16:05:45 +0700

    feat: install montserrat font

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,5 @@
+import defaultTheme from 'tailwindcss/defaultTheme'
+
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ['./resources/js/**/*.{ts,tsx,js,jsx}'],
@@ -14,6 +16,11 @@ export default {
         destructive: 'hsl(var(--destructive) / <alpha-value>)',
         warning: 'hsl(var(--warning) / <alpha-value>)',
       },
+    },
+    fontFamily: {
+      sans: ['Montserrat', 'arial', ...defaultTheme.fontFamily.sans],
+      serif: [...defaultTheme.fontFamily.serif],
+      mono: [...defaultTheme.fontFamily.mono],
     },
   },
   plugins: [],
