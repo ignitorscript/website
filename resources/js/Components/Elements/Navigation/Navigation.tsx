6359111f270afd6836d1ebe09335033fec67ae37
KimHout Sam <samkimhout@gmail.com>
2024-03-15 08:27:00 +0700

    feat: init navigation component

---
+++ resources/js/Components/Elements/Navigation/Navigation.tsx
@@ -0,0 +1,4 @@
+import { createReactContext } from '~/js/Utilities/createReactContext'
+import {} from '@ignition-concept/create-forwardref'
+
+const [NavigtionRoot, useNavigationRoot] = createReactContext('NavigationRoot')
